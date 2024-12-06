f98a3f1aa74136cdcb71b44aa6df78348dfee7f6
luke_venk <luke_venk@login4.stampede3.tacc.utexas.edu>
2024-12-06 14:26:51 -0600

    Update all files

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # High-Performance-Linear-Algebra
+
+MAKE SURE TO MAKE THE SCRIPT EXECUTABLE WITH
+*chmod +x build.sh*
--- build.sh
+++ build.sh
@@ -1,19 +1,7 @@
 #!/bin/bash
 
-# If the argument for the (exercise number) is not passed, throw error
-if [ -z "$1" ]; then
-    echo "Error: Please specify the exercise number (e.g., ./build.sh 1)"
-    exit 1
-fi
-
-# Set Exercise Number to the input, to be passed to CMakeLists.txt
-EXERCISE_NUMBER=$1
-
-# If the source file does not exist, throw error
-if [ ! -f "src/Exercise${EXERCISE_NUMBER}.cpp" ]; then 
-    echo "Error: Exercise${EXERCISE_NUMBER}.cpp does not exist!"
-    exit 1
-fi
+# MAKE SURE TO MAKE THE SCRIPT EXECUTABLE WITH
+# chmod +x build.sh
 
 # Set number of threads to 4, since our recursive approach uses 4 threads
 OMP_NUM_THREADS=4
@@ -24,13 +12,11 @@ mkdir -p build
 cd build
 
 # Run CMake with the specified Exercise Number
-cmake ../src -DEXERCISE_NUMBER=$EXERCISE_NUMBER
-
+cmake ../src
 # Build the project
 make
 
 # Run the newly compiled executable
-./Exercise$EXERCISE_NUMBER
+./hpla
 
-# MAKE SURE TO MAKE THE SCRIPT EXECUTABLE WITH
-# chmod +x build.sh
+
